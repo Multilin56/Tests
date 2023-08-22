eaf4d791657f7a255dd96ce95bb9e0332aed0a25
Multilin56 <104591081+Multilin56@users.noreply.github.com>
2023-08-22 20:01:20 +0300

    Create testN3.py

---
+++ testN3.py
@@ -0,0 +1,24 @@
+import math
+
+s = int(input("Введите макс. размер щита: "))
+i = int(input("Введите количество досок: "))
+
+#gg
+
+hmm = 0
+hmhmm = 0
+ahamm = 0
+answer = 0
+for i in range(i):
+    a = int(input("доска?:"))
+    ahamm -= 1
+    if (a == 1 and ahamm <= 0):
+        ahamm = s
+        hmhmm += 1
+    else:
+        answer += math.ceil(hmhmm/s)
+        hmhmm = 0
+else:
+    answer += math.ceil(hmhmm / s)
+
+print(answer)
