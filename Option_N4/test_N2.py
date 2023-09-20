dfc6642f74de24e09f9ba60098ac935677c943c8
Multilin56 <104591081+Multilin56@users.noreply.github.com>
2023-09-21 00:31:25 +0300

    Create test_N2.py

---
+++ Option_N4/test_N2.py
@@ -0,0 +1,13 @@
+import math
+
+n1 = int(input())
+n2 = int(input())
+
+# if (n1%2 == 0):
+#     print("Yes")
+#     if (n1%2 == 0):
+#         print((n1)/2, (n2*2)/2)
+#     else:
+#         print((n1)/2, (n2*2)/2)
+# else:
+#     print("No")
